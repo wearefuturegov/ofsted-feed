4bac4221de9430b8f42f4f40f53519b9e80a998f
David Carboni <david@carboni.io>
2020-05-01 10:30:00 +0100

    bashing the latest into the repo.

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const bodyParser = require('body-parser')
+const soap = require('node-soap')
+const fs = require('fs')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+function splitter_function(args) {
+    console.log('splitter_function');
+    var splitter = args.splitter;
+    var splitted_msg = args.message.split(splitter);
+    var result = [];
+    for(var i=0; i<splitted_msg.length; i++){
+      result.push(splitted_msg[i]);
+    }
+    return {
+        result: result
+        }
+}
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+var serviceObject = {
+    MessageSplitterService: {
+          MessageSplitterServiceSoapPort: {
+              MessageSplitter: splitter_function
+          },
+          MessageSplitterServiceSoap12Port: {
+              MessageSplitter: splitter_function
+          }
+      }
+  };
+  
+  // load the WSDL file
+  var xml = fs.readFileSync('soap/WebExtractServices.wsdl', 'utf8');
+  // create express app
+  var app = express();
+  
+  // root handler
+  app.get('/', function (req, res) {
+    res.send('Node Soap Example!<br /><a href="https://github.com/macogala/node-soap-example#readme">Git README</a>');
+  })
+  
+  var port = 8000;
+  // Launch the server and listen on *port*
+  app.listen(port, function () {
+    console.log('Listening on port ' + port);
+    var wsdl_path = "/wsdl";
+    // create SOAP server that listens on *path*
+    soap.listen(app, wsdl_path, serviceObject, xml);
+    console.log("Check http://localhost:" + port + wsdl_path +"?wsdl to see if the service is working");
+  });
