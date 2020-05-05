3ab32334f93df8e14ba7055320cd900c57afe900
David Carboni <david@carboni.io>
2020-05-05 17:52:28 +0100

    Added a curl to the end of the deployment.

--- function/deploy.sh
+++ function/deploy.sh
@@ -62,3 +62,8 @@ options="--region=europe-west2 --memory=256MB --trigger-http --allow-unauthentic
 gcloud functions deploy feed --runtime=nodejs10 --set-env-vars TOKEN=${token} ${options} \
     --vpc-connector ofsted-egress-vpcc \
     --egress-settings all
+
+# Quick test...
+echo "Breathe..."
+sleep 5
+curl -v https://europe-west2-${project_id}.cloudfunctions.net/feed?token=${token}
