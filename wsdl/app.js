6ff2990f0c13a1a9b12b5796a7976622f25e2ae0
David Carboni <david@carboni.io>
2020-05-29 17:04:22 +0100

    Added downloads of xsd0 and xsd1

--- wsdl/app.js
+++ wsdl/app.js
@@ -1,8 +1,19 @@
 // Retrieves the Ofsted WSDL file from the development environment.
 
 const axios = require('axios');
+const format = require('xml-formatter');
+const fs = require('fs');
+const tmp = require('tmp');
+const {Storage} = require('@google-cloud/storage');
 
-var url = 'https://testinfogateway.ofsted.gov.uk/ISPPGateway/ISPPGatewayServices.svc?wsdl';
+const urlWsdl = 'https://testinfogateway.ofsted.gov.uk/ISPPGateway/ISPPGatewayServices.svc?wsdl';
+const urlXsd0 = 'https://testinfogateway.ofsted.gov.uk/ISPPGateway/ISPPGatewayServices.svc?xsd=xsd0';
+const urlXsd1 = 'https://testinfogateway.ofsted.gov.uk/ISPPGateway/ISPPGatewayServices.svc?xsd=xsd1';
+
+const bucketName = "ofsted-feed-soap"
+const storage = new Storage();
+const bucket = storage.bucket(bucketName);
+
 
 exports.wsdl = async (req, res) => {
 
@@ -10,18 +21,30 @@ exports.wsdl = async (req, res) => {
     res.send(401, 'please provide a ?token= parameter value.')
   }
 
-  try {
-    const response = await axios.get(url);
-    console.log(response);
-    xml = response.data
+  response = await axios.get(urlXsd0);
+  xml = response.data
+  save("xsd0.xml", xml)
 
-    res
-      .set("Content-Type", "text/xml; charset=utf8")
-      .status(200)
-      .send(xml);
+  response = await axios.get(urlXsd1);
+  xml = response.data
+  save("xsd1.xml", xml)
 
-  } catch (error) {
-    console.error(error);
-  }
+  response = await axios.get(urlWsdl);
+  xml = response.data
+  save("wsdl.xml", xml)
 
+  // Return the wsdl
+  res
+    .set("Content-Type", "text/xml; charset=utf8")
+    .status(200)
+    .send(format(xml));
+}
+
+
+async function save(name, xml) {
+  formatted = format(xml);
+  file = tmp.fileSync();
+  fs.writeFileSync(file.name, formatted)
+  await storage.bucket(bucketName).upload(file.name, {destination: `development/${name}`})
+  console.log(`XML saved to: ${bucketName}/${name}`);
 }
