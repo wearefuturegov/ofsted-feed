e80181fe513525324a21676dc26ff85a798f2159
David Carboni <david@carboni.io>
2020-06-12 22:33:54 +0100

    Adding WSDL as an environment variable.

--- feed/deploy.sh
+++ feed/deploy.sh
@@ -11,11 +11,13 @@ project_number=$(gcloud projects describe $project_id --format="value(projectNum
 # NB: outbound requests are routed through the VPC so they have a static IP address
 token=$(cat ../credentials/token.txt)
 options="--region=europe-west2 --memory=256MB --trigger-http --allow-unauthenticated"
-gcloud functions deploy feed --runtime=nodejs10 --set-env-vars TOKEN=${token},PROJECT_NUMBER=${project_number} ${options} \
+#wsdl="https://testinfogateway.ofsted.gov.uk/ISPPGateway/ISPPGatewayServices.svc?wsdl"
+wsdl="./wsdl.xml"
+gcloud functions deploy feed --runtime=nodejs10 --set-env-vars TOKEN=${token},PROJECT_NUMBER=${project_number},WSDL=${wsdl} ${options} \
     --vpc-connector ofsted-egress-vpcc \
     --egress-settings all
 
 # Quick test...
 echo "Breathe..."
 sleep 2
-curl -v https://europe-west2-${project_id}.cloudfunctions.net/wsdl?token=${token}
+curl -v https://europe-west2-${project_id}.cloudfunctions.net/feed?token=${token}
