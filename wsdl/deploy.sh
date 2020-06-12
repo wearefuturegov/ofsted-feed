71f0ca043a15a5e382cbc82edfe9b4439e6468a8
David Carboni <david@carboni.io>
2020-06-12 18:41:28 +0100

    Consolidating deployment scripts to run from the root directory.

--- wsdl/deploy.sh
+++ wsdl/deploy.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 
+# This script should be called by ../deploy.sh unless you're all set up and configured and just need to redeploy this function.
+
 # Account and GCP project
-project_id=$(cat project.txt)
-account=$(cat account.txt)
-
-# Setup
-gcloud config set account $account
-gcloud config set project $project_id
-
-# Enable APIs
-gcloud services enable cloudfunctions.googleapis.com
+project_id=$(cat ../credentials/project.txt)
 
 # Ofsted wsdl function
 # NB: outbound requests are routed through the VPC so they have a static IP address
-token=$(cat token.txt)
+token=$(cat ../credentials/token.txt)
 options="--region=europe-west2 --memory=256MB --trigger-http --allow-unauthenticated"
 gcloud functions deploy wsdl --runtime=nodejs10 --set-env-vars TOKEN=${token} ${options} \
     --vpc-connector ofsted-egress-vpcc \
