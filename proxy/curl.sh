e231c1b8073f0c43f7ac73abc26a5812ae3d94c0
David Carboni <david@carboni.io>
2020-07-03 17:00:43 +0100

    Tweaking for temporary test.

--- proxy/curl.sh
+++ proxy/curl.sh
@@ -4,7 +4,10 @@ set -euf -o pipefail
 project_id=$(gcloud config get-value project)
 token=$(cat ../credentials/token.txt)
 
-url=https://europe-west2-${project_id}.cloudfunctions.net/feed?token=${token}
+# url=https://europe-west2-${project_id}.cloudfunctions.net/feed?token=${token}
+url=https://europe-west2-${project_id}.cloudfunctions.net/proxy?token=test
 
 echo Calling: $url
-curl -v $url
+#curl -v $url
+
+curl -X post -d @"../secrets/example-messages/Fiddler SOAP Request - FIDY.xml" -H "content-type: text/xml" $url
