52979ac88fb205750074e404f86ab786aa04ac24
David Carboni <david@carboni.io>
2020-07-03 18:46:17 +0100

    Script to build and run the container locally.

---
+++ ofsted-client/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+docker build --tag ofsted-client .
+
+parentdir=$(dirname "$PWD")
+keyfile=${parentdir}/secrets/run-key.json
+certfolder=${parentdir}/secrets/cert
+project_id=$(gcloud config get-value project)
+project_number=$(gcloud projects describe $project_id --format="value(projectNumber)")
+function=europe-west2-${project_id}.cloudfunctions.net/proxy
+
+docker run -it --rm -p 8080:8080 \
+  -v "${keyfile}":/secrets/key \
+  --env GOOGLE_APPLICATION_CREDENTIALS="/secrets/key" \
+  --env PROJECT_NUMBER="${project_number}" \
+  --env FUNCTION="${function}" \
+  ofsted-client
+
