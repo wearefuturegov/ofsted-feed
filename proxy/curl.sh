d226f39c75d099aaedb5b8c5fe533a95d51ec86c
David Carboni <david@carboni.io>
2020-07-03 17:12:28 +0100

    Added verbose.

--- proxy/curl.sh
+++ proxy/curl.sh
@@ -2,7 +2,7 @@
 set -euf -o pipefail
 
 project_id=$(gcloud config get-value project)
-token=$(cat ../credentials/token.txt)
+token=$(cat ../secrets/token.txt)
 
 # url=https://europe-west2-${project_id}.cloudfunctions.net/feed?token=${token}
 url=https://europe-west2-${project_id}.cloudfunctions.net/proxy?token=test
@@ -10,4 +10,4 @@ url=https://europe-west2-${project_id}.cloudfunctions.net/proxy?token=test
 echo Calling: $url
 #curl -v $url
 
-curl -X post -d @"../secrets/example-messages/Fiddler SOAP Request - FIDY.xml" -H "content-type: text/xml" $url
+curl -v -X post -d @"../secrets/example-messages/Fiddler SOAP Request - FIDY.xml" -H "content-type: text/xml" $url
