171f54de881fddcbd17c114cb02a4250b0fa7133
David Carboni <david@carboni.io>
2020-07-17 18:22:16 +0100

    Pretty close in matching XML to the correct format. Looks like the wrong values are being signed though, so we're not yet able to make a successful request.

--- ofsted-client/app.py
+++ ofsted-client/app.py
@@ -1,19 +1,22 @@
 
-from flask import Flask, Response, abort, request
+from flask import Flask, Response, abort, request, current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 
 import requests
-from zeep import Client
+from zeep import Client, xsd
 from zeep.wsse.username import UsernameToken
 from zeep.wsse.signature import Signature
 from zeep.wsse.signature import BinarySignature
 from zeep.wsse import utils
+from zeep.wsa import WsAddressingPlugin
 from xml.dom import minidom
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
+from lxml import etree
 from datetime import datetime, timedelta
 from google.cloud import secretmanager
 import os
 import tempfile
+import base64
 
 app = Flask(__name__)
 
@@ -21,22 +24,21 @@ function_url = os.getenv('FUNCTION_URL')
 
 @app.route('/')
 def call():
-
-    print("Anything?")
     
+    # Get the access token secret
     access_token = get_secret("access_token")
     if access_token.strip():
         print("Got an access token ok")
     else:
         print("Got an empty access token")
 
-    # Check the access token
+    # Check the access token value
     token = request.args.get('token')
     if not token or token != access_token:
-        print("nah...")
+        print("Token not matched")
         abort(401)
     else:
-        print("Let's go!")
+        print("Requesting feed")
         return get_feed()
 
 
@@ -45,26 +47,137 @@ def get_feed():
     user_name_token = username_password()
     signature = binary_signature_timestamp()
 
-    client = Client('tweaked_xml/wsdl.xml', wsse=[user_name_token, signature])
+    client = Client('tweaked_xml/wsdl.xml', wsse=[user_name_token, signature], plugins=[WsAddressingPlugin()])
+    # client.set_ns_prefix('', 'http://information.gateway.ofsted.gov.uk/ispp')
+    # client.set_ns_prefix('s', 'http://www.w3.org/2003/05/soap-envelope')
+    client.set_ns_prefix('a', 'http://www.w3.org/2005/08/addressing')
+    client.set_ns_prefix('u', 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd')
     parameters = {
         'LocalAuthorityCode': 825
       }
     
+    # Add a "ReplyTo/Address"
+    header = xsd.Element(
+        '{http://www.w3.org/2005/08/addressing}ReplyTo',
+        xsd.ComplexType([
+            xsd.Element(
+                '{http://www.w3.org/2005/08/addressing}Address',
+                xsd.String()),
+        ])
+    )
+    header_value = header(Address='http://www.w3.org/2005/08/addressing/anonymous')
+    
     # Request document
-    node = client.create_message(client.service, 'GetLocalAuthorityChildcareRegister', localAuthorityRequest=parameters)
-    xmlstr = minidom.parseString(ET.tostring(node)).toprettyxml(indent="   ")
-    print(xmlstr)
+    envelope = client.create_message(client.service, 'GetLocalAuthorityChildcareRegister', localAuthorityRequest=parameters, _soapheaders=[header_value])
+    
+    # Update a few tricky values
 
-    response = call_proxy(node)
+    header = envelope.find('{http://www.w3.org/2003/05/soap-envelope}Header')
+    security = header.find('{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}Security')
+    signature = security.find('{http://www.w3.org/2000/09/xmldsig#}Signature')
+    signature_value = signature.find('{http://www.w3.org/2000/09/xmldsig#}SignatureValue')
+    binary_security_token = security.find('{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}BinarySecurityToken')
 
-    if response.status_code != 200:
-        print(f"{response.status_code}: {response.text}")
+    # Reformat base-64 values without line-wrapping 
+    # # (possible the server isn't reading them correctly otherwise)
+    nowrapping(signature_value)
+    nowrapping(binary_security_token)
+
+    # Sprinkle in some 'mustUnderstand' just in case
+    mustunderstanders = [
+            '{http://www.w3.org/2005/08/addressing}Action', 
+            '{http://www.w3.org/2005/08/addressing}To', 
+            '{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}Security'
+            ]
+    for child in header:
+        if child.tag in mustunderstanders:
+            print(f"Adding mustUnderstand to {child.tag}")
+            child.attrib['{http://www.w3.org/2003/05/soap-envelope}mustUnderstand']="1"
+    
+    # Pop in a couple of missing ids
+    for child in header:
+        if child.tag == '{http://www.w3.org/2005/08/addressing}To':
+            child.attrib['{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Id'] = "_1"
+    for child in security:
+        if child.tag == '{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}UsernameToken':
+            child.attrib['{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Id'] = "_2"
+    
+    # Delete duplicated elements
+    duplicates = ['{http://www.w3.org/2005/08/addressing}Action', '{http://www.w3.org/2005/08/addressing}To', '{http://www.w3.org/2005/08/addressing}MessageID']
+    deduped = {}
+    insert_index = 0
+    for child in header:
+        if child.tag in duplicates:
+            deduped[child.tag] = child
+            header.remove(child)
+        if child.tag == '{http://www.w3.org/2005/08/addressing}ReplyTo':
+            insert_index = header.index(child)
+
+    # Re-insert the de-duped elements
+    for child in deduped.values():
+        insert_index = insert_index + 1
+        header.insert(insert_index, child)
+
+    for child in header:
+        print(f' - {child.tag[child.tag.rindex("}")+1:]}  {child.tag}  -->  {child.attrib}')
+        if child.tag=='{http://www.w3.org/2005/08/addressing}ReplyTo':
+            print(header.index(child))
+    
+    # xmlstr =  minidom.parseString(ET.tostring(node)).toprettyxml(indent="   ")
+    xmlstr = str(etree.tostring(envelope, encoding='unicode', pretty_print=True))
+    print(type(xmlstr))
+
+    # xmlstr=tweak(xmlstr)
+
+    ##print(xmlstr)
+    if os.path.isdir('/output'):
+        with open('/output/actual1.xml', 'w+') as f:
+            f.write(xmlstr)
+            print("Saved generated XML message.")
+    if os.path.isdir('../secrets/compare'):
+        with open('../secrets/compare/actual1.xml', 'w+') as f:
+            f.write(xmlstr)
+            print("Saved generated XML message.")
+
+    # response = call_proxy(envelope)
+
+    # if response.status_code != 200:
+    #     print(f"{response.status_code}: {response.text}")
 
     #return minidom.parseString(response.text)
     #ET.tostring(feed, encoding='unicode').toprettyxml(indent="   ")
 
     #return Response(f"{response.status_code} -- {response.text}"), 200
-    return Response(response.text, mimetype='text/xml'), response.status_code
+    # return Response(response.text, mimetype='text/xml'), response.status_code
+    return Response(str(etree.tostring(envelope, encoding='unicode', pretty_print=True)), mimetype='text/xml'), 200
+
+
+# def tweak(xmlstr):
+#     result = xmlstr
+#     # result = renamespace('soap-env', 's', result)
+#     # result = renamespace('ns0', 's', result)
+#     # result = renamespace('wsa', 'a', result)
+#     # result = renamespace('ns1', 'a', result)
+#     # result = renamespace('wsse', 'o', result)
+#     # result = renamespace('wsu', 'u', result)
+#     # result = renamespace('ns4', 'u', result)
+#     # result = result.replace('<a:Action>', '<a:Action s:mustUnderstand="1">')
+#     # result = result.replace('<a:To>', '<a:To s:mustUnderstand="1" u:Id="_1">')
+#     # result = result.replace('<o:Password Type="', '<o:Password o:Type="')
+#     return result
+
+# def renamespace(current, updated, xmlstr):
+#     result = xmlstr
+#     result = result.replace(f'xmlns:{current}', f'xmlns:{updated}')
+#     result = result.replace(f'{current}:', f'{updated}:')
+#     return result
+
+def nowrapping(element):
+    """ Updates the text of the given element so that there are no line-breaks in the base-64 text value. """
+    element_text = element.text
+    data = base64.b64decode(element_text)
+    b64 = base64.b64encode(data)
+    element.text = b64
 
 
 class BinarySignatureTimestamp(BinarySignature):
@@ -87,7 +200,7 @@ class BinarySignatureTimestamp(BinarySignature):
 def username_password():
 
     username = get_secret('username')
-    password = get_secret('username')
+    password = get_secret('password')
     return UsernameToken(username, password)
 
 
@@ -113,17 +226,22 @@ def binary_signature_timestamp():
 
 def get_secret(name):
 
-    print(f"Getting GCP secret for {name}")
-    PROJECT_NUMBER = os.environ.get("PROJECT_NUMBER")
-    secrets = secretmanager.SecretManagerServiceClient()
-    return secrets.access_secret_version(f"projects/{PROJECT_NUMBER}/secrets/{name}/versions/latest").payload.data.decode("utf-8")
+    if not name in current_app.config:
+
+        print(f"Getting GCP secret for {name}")
+        PROJECT_NUMBER = os.environ.get("PROJECT_NUMBER")
+        secrets = secretmanager.SecretManagerServiceClient()
+        current_app.config[name] = secrets.access_secret_version(f"projects/{PROJECT_NUMBER}/secrets/{name}/versions/latest").payload.data.decode("utf-8")
+
+    return current_app.config[name]
 
 
 def call_proxy(xml):
 
     # Provide the token in the request to the receiving function
     headers = {'Authorization': f'bearer {jwt()}'}
-    body = ET.tostring(xml, encoding='unicode')
+    body = etree.tostring(xml, encoding='utf-8', xml_declaration=True)
+    # body = ET.tostring(xml, encoding='unicode')
     response = requests.post(function_url, headers=headers, data=body)
     return response
 
--- ofsted-client/run.sh
+++ ofsted-client/run.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-docker build --tag ofsted-client .
+# docker build --tag ofsted-client .
 
 parentdir=$(dirname "$PWD")
 keyfile=${parentdir}/secrets/run-key.json
 certfolder=${parentdir}/secrets/cert
+messagefolder=${parentdir}/secrets/compare
 project_id=$(gcloud config get-value project)
 project_number=$(gcloud projects describe $project_id --format="value(projectNumber)")
 function="https://europe-west2-${project_id}.cloudfunctions.net/proxy"
 
-docker run -it --rm -p 8080:8080 \
-  -v "${keyfile}":/secrets/key \
-  --env GOOGLE_APPLICATION_CREDENTIALS="/secrets/key" \
-  --env PROJECT_NUMBER="${project_number}" \
-  --env FUNCTION_URL="${function}" \
-  --env FUNCTION_TOKEN=$(gcloud auth print-identity-token) \
-  ofsted-client
+# docker run -it --rm -p 8080:8080 \
+#   -v "${messagefolder}":/output \
+#   -v "${keyfile}":/secrets/key \
+#   --env GOOGLE_APPLICATION_CREDENTIALS="/secrets/key" \
+#   --env PROJECT_NUMBER="${project_number}" \
+#   --env FUNCTION_URL="${function}" \
+#   --env FUNCTION_TOKEN=$(gcloud auth print-identity-token) \
+#   --user $(id -u ${USER}):$(id -g ${USER}) \
+#   ofsted-client
 
+export GOOGLE_APPLICATION_CREDENTIALS=${keyfile}
+export PROJECT_NUMBER=${project_number}
+export FUNCTION_URL="${function}"
+export FLASK_ENV=development
+export FUNCTION_TOKEN=$(gcloud auth print-identity-token)
+flask run
